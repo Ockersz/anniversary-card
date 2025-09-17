3eb6688016fb856908427e658822177ae917209b
Ockersz <shaheinockersz1234@gmail.com>
2025-09-17 15:14:54 +0530

    fix: Update allowedHosts in Vite configuration for preview settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 8080,                // must match DO health check
-    allowedHosts: ['*'], // add your DO domain here
+    port: 8080,
+    allowedHosts: [
+      "anni-app-krkoi.ondigitalocean.app",
+      "anni-app-*.ondigitalocean.app",
+    ],
   },
-})
+});
